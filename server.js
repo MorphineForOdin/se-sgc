924580a230ace57f02edb17e4dd03c845f06903a
MorphineForOdin <svyatoslav.malinovskyj@gmail.com>
2020-08-08 11:55:49 +0300

    Recent articles route

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => {
-    res.send('SE-SGC');
-})
+const articleRouter = require('./routes/articles');
+
+app.set('view engine', 'ejs');
+app.use('/articles', articleRouter);
+
+function getRecentArticles(take) {
+    let articles = [];
+    while (take > 0)
+        articles.push({
+            title: `Title ${--take}`,
+            createdDate: new Date(),
+            description: `Description ${take}`
+        });
+    return articles;
+}
+app.get('/', (req, res) => res.render('index', {
+    articles: getRecentArticles(10)
+}));
+
 app.listen(5000);
