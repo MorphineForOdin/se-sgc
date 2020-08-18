19e454db6b9b37de5481a44b53033ea660fc1e5e
MorphineForOdin <svyatoslav.malinovskyj@gmail.com>
2020-08-18 08:34:55 +0300

    Simple mobile header menu.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,7 @@
 console.log('JS uploaded...');
+
+const toggleBtn = document.getElementsByClassName('toggle-button')[0];
+const navLinks = document.getElementsByClassName('navigation-links')[0];
+
+if (toggleBtn)
+    toggleBtn.addEventListener('click', () => navLinks.classList.toggle('active'));
