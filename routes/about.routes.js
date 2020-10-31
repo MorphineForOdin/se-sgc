4a41f08c08b050ac39cbf70306446ca6f36ab680
MorphineForOdin <svyatoslav.malinovskyj@gmail.com>
2020-10-31 16:36:43 +0200

    Site's style guide page.

--- routes/about.routes.js
+++ routes/about.routes.js
@@ -3,4 +3,6 @@ const router = express.Router();
 
 router.get('/', async (req, res) => res.render('about/index'));
 
+router.get('/styles', (req, res) => res.render('about/styles'));
+
 module.exports = router;
